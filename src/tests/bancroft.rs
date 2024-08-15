86f74c4ee8b0eb3f6f2fde5ef701f2ec6b2d7c9a
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-08-15 18:34:59 +0200

    clean up format
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/tests/bancroft.rs
+++ src/tests/bancroft.rs
@@ -9,71 +9,55 @@ use std::str::FromStr;
 
 #[test]
 fn test() {
+    let mut pool = Vec::<Candidate>::new();
     let t0 = Epoch::from_str("2020-06-25T00:00:00 GPST").unwrap();
-    let t0 = Epoch::default();
     let (x0, y0, z0) = (3582105.291, 532589.7313, 5232754.8054);
+    for (i, (pr, dt, sv_x_m, sv_y_m, sv_z_m)) in [
+        (
+            28776032.260,
+            Duration::from_microseconds(142.784),
+            24170352.34904016,
+            -16029029.85873581,
+            -5905924.153143198,
+        ),
+        (
+            24090441.364,
+            Duration::from_microseconds(-313.533),
+            16069642.946692571,
+            -8992001.827692423,
+            23184746.654093638,
+        ),
+        (
+            24762903.616,
+            Duration::from_microseconds(-368.749),
+            26119621.94656989,
+            7791422.617964384,
+            11558902.718228433,
+        ),
+        (
+            25537644.454,
+            Duration::from_milliseconds(-6.158955),
+            -3601205.0295727667,
+            -20311399.087870672,
+            21230831.216778148,
+        ),
+    ]
+    .iter()
+    .enumerate()
+    {
+        let pr = Observation::pseudo_range(Carrier::E1, *pr, None);
+        let mut cd = Candidate::new(SV::new(Constellation::default(), i as u8), t0, vec![pr]);
+        cd.set_clock_correction(ClockCorrection::without_relativistic_correction(*dt));
+        cd.set_orbit(Orbit::from_position(
+            *sv_x_m,
+            *sv_y_m,
+            *sv_z_m,
+            t0,
+            EARTH_J2000,
+        ));
+        pool.push(cd);
+    }
 
-    let pr = Observation::pseudo_range(Carrier::E1, 28776032.260, None);
-
-    let mut cd0 = Candidate::new(SV::new(Constellation::default(), 2), t0, vec![pr]);
-
-    cd0.set_clock_correction(ClockCorrection::without_relativistic_correction(
-        Duration::from_microseconds(142.784),
-    ));
-    cd0.set_orbit(Orbit::from_position(
-        24170352.34904016 / 1.0E3,
-        -16029029.85873581 / 1.0E3,
-        -5905924.153143198 / 1.0E3,
-        t0,
-        EARTH_J2000,
-    ));
-
-    let pr = Observation::pseudo_range(Carrier::E1, 24090441.364, None);
-
-    let mut cd1 = Candidate::new(SV::new(Constellation::default(), 3), t0, vec![pr]);
-
-    cd1.set_clock_correction(ClockCorrection::without_relativistic_correction(
-        Duration::from_microseconds(-313.533),
-    ));
-    cd1.set_orbit(Orbit::from_position(
-        16069642.946692571 / 1.0E3,
-        -8992001.827692423 / 1.0E3,
-        23184746.654093638 / 1.0E3,
-        t0,
-        EARTH_J2000,
-    ));
-
-    let pr = Observation::pseudo_range(Carrier::E1, 24762903.616, None);
-
-    let mut cd2 = Candidate::new(SV::new(Constellation::default(), 5), t0, vec![pr]);
-
-    cd2.set_clock_correction(ClockCorrection::without_relativistic_correction(
-        Duration::from_microseconds(-368.749),
-    ));
-    cd2.set_orbit(Orbit::from_position(
-        26119621.94656989 / 1.0E3,
-        7791422.617964384 / 1.0E3,
-        11558902.718228433 / 1.0E3,
-        t0,
-        EARTH_J2000,
-    ));
-
-    let pr = Observation::pseudo_range(Carrier::E1, 25537644.454, None);
-
-    let mut cd3 = Candidate::new(SV::new(Constellation::default(), 8), t0, vec![pr]);
-
-    cd3.set_clock_correction(ClockCorrection::without_relativistic_correction(
-        Duration::from_milliseconds(-6.158955),
-    ));
-    cd3.set_orbit(Orbit::from_position(
-        -3601205.0295727667 / 1.0E3,
-        -20311399.087870672 / 1.0E3,
-        21230831.216778148 / 1.0E3,
-        t0,
-        EARTH_J2000,
-    ));
-
-    let pool = vec![cd0, cd1, cd2, cd3];
     let solver = Bancroft::new(&pool);
     assert!(
         solver.is_ok(),
@@ -92,11 +76,11 @@ fn test() {
     let x_err = (output[0] - x0).abs();
     let y_err = (output[1] - y0).abs();
     let z_err = (output[2] - z0).abs();
-    //assert!(
-    //    x_err < 100.0,
-    //    "bancroft solver error: x error too large: {}",
-    //    x_err
-    //);
+    assert!(
+        x_err < 100.0,
+        "bancroft solver error: x error too large: {}",
+        x_err
+    );
     assert!(
         y_err < 100.0,
         "bancroft solver error: y error too large: {}",
