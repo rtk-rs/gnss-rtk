625172b04b83714c9ea1f4525eb2e2c185e98925
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-05-01 14:34:01 +0200

    add more tests
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/tests/pool.rs
+++ src/tests/pool.rs
@@ -1,9 +1,10 @@
 use crate::{
     pool::Pool,
-    prelude::{Config, Epoch, Frame},
+    prelude::{Config, Epoch, Frame, TimeScale},
     tests::{data::CandidatesBuilder, init_logger, time::NullTime, OrbitsData},
 };
 
+use hifitime::Duration;
 use rstest::*;
 use std::str::FromStr;
 
@@ -50,6 +51,78 @@ fn test_simple_pool_fit() {
     assert_eq!(
         pool.candidates().len(),
         candidates.len(),
-        "builder dropped some data!"
+        "pool fit dropplled some data!"
     );
+}
+
+#[test]
+fn test_pool_gst_preference() {
+    init_logger();
+
+    let null_time = NullTime {};
+
+    let mut default_cfg = Config::default();
+    default_cfg.timescale = TimeScale::GST;
+
+    let earth_frame = build_earth_frame();
+    let mut orbits_data = build_orbit_source();
+
+    let t0_gpst = Epoch::from_str("2020-06-25T00:00:00 GPST").unwrap();
+
+    let candidates = CandidatesBuilder::build_at(t0_gpst);
+
+    let mut pool = Pool::allocate(0, earth_frame);
+
+    pool.new_epoch(&candidates);
+    pool.pre_fit(&default_cfg, &null_time);
+
+    assert_eq!(
+        pool.candidates().len(),
+        candidates.len(),
+        "pool prefit dropped some data!"
+    );
+
+    for cd in pool.candidates().iter() {
+        assert_eq!(
+            cd.t.time_scale,
+            TimeScale::GST,
+            "{}({}) - not correctly transposed to GST",
+            cd.t,
+            cd.sv
+        );
+    }
+
+    pool.orbital_states(&default_cfg, &mut orbits_data);
+
+    for cd in pool.candidates().iter() {
+        assert_eq!(
+            cd.t.time_scale,
+            TimeScale::GST,
+            "{}({}) - not correctly transposed to GST",
+            cd.t,
+            cd.sv
+        );
+        assert_eq!(
+            cd.t_tx.time_scale,
+            TimeScale::GST,
+            "{}({}) - not correctly transposed to GST",
+            cd.t_tx,
+            cd.sv
+        );
+
+        assert!(
+            cd.t > cd.t_tx,
+            "{}({}) - physical non sense (after transposition to GST)",
+            cd.t,
+            cd.sv
+        );
+
+        let dt = cd.t - cd.t_tx;
+        assert!(
+            dt < Duration::from_milliseconds(100.0),
+            "{}({}) - not compatible with MEO/LEO Earth nav (after transposition to GST)",
+            cd.t,
+            cd.sv
+        );
+    }
 }
